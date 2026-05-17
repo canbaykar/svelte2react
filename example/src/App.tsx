d02509b2a2cc64046affd3b7e23098051f992ce3
Can <togancan42@gmail.com>
2026-05-17 08:31:46 +0300

    docs: simplify example and fix example dependency
    The project svelte2react was originally named rollup-plugin-svelte2react, and the example imported it by that name. This is fixed. Also example is greatly simplified.

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,42 +1,25 @@
-import reactLogo from './assets/react.svg';
-import viteLogo from './assets/vite.svg';
-import heroImg from './assets/hero.png';
-import './App.css';
-
 import { useState } from 'react';
+import { Wrap } from '@baykar/svelte2react';
 
-import Counter from './Counter';
-import Counter2 from './Counter2.svelte';
-import { Wrap } from '@baykar/rollup-plugin-svelte2react';
-// import { Wrap } from './Wrapper.svelte';
+import ReactButton from './ReactButton';
+import _SvelteButton from './SvelteButton.svelte';
+const SvelteButton = Wrap(_SvelteButton);
 
-const CounterSvelte = Wrap(Counter2);
+export default function App() {
+  const [name, setName] = useState('Mark');
 
-function App() {
-  const [suffix, setSuffix] = useState(0);
+  return <section>
+    <h1>React <em>vs</em> Wrapped Svelte</h1>
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <Counter name={'React' + suffix} />
-        <CounterSvelte name={'Svelte' + suffix} />
-        <button onClick={() => {
-            setSuffix(s => s + 1);
-          }}>Increment nameCount</button>
-      </section>
-    </>
-  )
+    <label>
+      Your name:
+      <input
+        value={name}
+        onChange={e => setName(e.target.value)}
+      />
+    </label>
+
+    <ReactButton name={name} />
+    <SvelteButton name={name} />
+  </section>;
 }
-
-export default App
